07161214a97e4b132870e1c2299bbd06962fec5e
lvBingo <706610514@qq.com>
2015-03-12 06:10:04 +0800

    修改细节

---
+++ static/js/wapsite/angelcrunch.weixintobrowser.jq.c.js
@@ -0,0 +1,34 @@
+﻿/*/
+/*
+/* Rank: 3
+/* Wap site cookie operation
+/* Rely on jquery files
+/*
+/*/
+
+// Module Initialize
+$(function () {
+    (function () {
+        var ua = navigator.userAgent.toLowerCase();
+        var isAndroid = ua.indexOf("android") != -1 ? 1 : 0
+        , isWeixin = navigator.userAgent.toLowerCase().indexOf("micromessenger") != -1 ? 1 : 0;
+
+        if (isWeixin) {
+            if (isAndroid) $("#wechat-tag").removeClass("download-ios").addClass("download-android");
+            $(".wechat2browser").click(function () {
+                if ($(this).attr("data-donot-showinwechat")
+                    && !$("#wechat-tag").hasClass("download-android")) {
+                    $(this).attr("data-confirm-donot-redirect", "true");
+                    return false;
+                }
+                $("#wechat-tag").fadeIn();
+                $(this).attr("data-confirm-donot-redirect", "true");
+                return false;
+            });
+        }
+
+        $("#wechat-tag").click(function () {
+            $(this).fadeOut();
+        })
+    }).call(this);
+})
